79e7f2d7a9a30894c1c2877d5f4a0249c0f5ab60
mnmhbbb <mnmhb64@gmail.com>
2024-08-13 00:18:21 +0900

    next-pwa 설치 및 기본 설정 적용

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import nextPwa from "next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+const withPWA = nextPwa({
+  dest: "public",
+  register: true,
+});
+
+const config = withPWA({
+  ...nextConfig,
+});
+
+export default config;
