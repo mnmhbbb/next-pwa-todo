3dcf073f8a38dbe5bc80bf6f73836b62509bde18
mnmhbbb <mnmhb64@gmail.com>
2024-08-14 00:19:05 +0900

    pwa 기본 설정 수정

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {};
 const withPWA = nextPwa({
   dest: "public",
   register: true,
+  skipWaiting: true,
+  // disable: process.env.NODE_ENV === 'development'
+  // customWorkerDir: "worker",
+  // runtimeCaching,
 });
 
 const config = withPWA({
