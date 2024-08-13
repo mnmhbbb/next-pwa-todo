7041c496c80de19c230c24b1e9e9843a5faca500
mnmhbbb <mnmhb64@gmail.com>
2024-08-13 21:31:04 +0900

    🧪 스플래쉬 테스트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,53 +39,6 @@ export const metadata: Metadata = {
     description: "그래도 해야지 어떡해",
     images: ["https://next-pwa-todo.vercel.app/assets/icons/og.png"],
   },
-  appleWebApp: {
-    title: "abc",
-    statusBarStyle: "black-translucent",
-    startupImage: [
-      "https://next-pwa-todo.vercel.app/assets/icons/icon-192x192.png",
-      {
-        url: "https://next-pwa-todo.vercel.app/assets/splashscreens/iphone5_splash.png",
-        media: "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)",
-      },
-      {
-        url: "https://next-pwa-todo.vercel.app/assets/splashscreens/iphone6_splash.png",
-        media: "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)",
-      },
-      {
-        url: "https://next-pwa-todo.vercel.app/assets/splashscreens/iphoneplus_splash.png",
-        media: "(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)",
-      },
-      {
-        url: "https://next-pwa-todo.vercel.app/assets/splashscreens/iphonex_splash.png",
-        media: "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)",
-      },
-      {
-        url: "https://next-pwa-todo.vercel.app/assets/splashscreens/iphonexr_splash.png",
-        media: "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)",
-      },
-      {
-        url: "https://next-pwa-todo.vercel.app/assets/splashscreens/iphonexsmax_splash.png",
-        media: "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)",
-      },
-      {
-        url: "https://next-pwa-todo.vercel.app/assets/splashscreens/ipad_splash.png",
-        media: "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)",
-      },
-      {
-        url: "https://next-pwa-todo.vercel.app/assets/splashscreens/ipadpro1_splash.png",
-        media: "(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)",
-      },
-      {
-        url: "https://next-pwa-todo.vercel.app/assets/splashscreens/ipadpro3_splash.png",
-        media: "(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)",
-      },
-      {
-        url: "https://next-pwa-todo.vercel.app/assets/splashscreens/ipadpro2_splash.png",
-        media: "(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)",
-      },
-    ],
-  },
 };
 
 export default function RootLayout({
