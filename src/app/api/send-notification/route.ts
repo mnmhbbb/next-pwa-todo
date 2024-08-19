ab3ab4a557d1f852cc9d8c654acc515c59e2b87f
mnmhbbb <66292371+mnmhbbb@users.noreply.github.com>
2024-08-19 07:34:40 +0000

    푸시 테스트3

--- src/app/api/send-notification/route.ts
+++ src/app/api/send-notification/route.ts
@@ -8,6 +8,14 @@ const privateVapidKey = process.env.VAPID_PRIVATE_KEY!;
 
 webPush.setVapidDetails('mailto:rachaenlee@gmail.com', publicVapidKey, privateVapidKey);
 
+interface SubscriptionType {
+    endpoint: string;
+    keys: {
+      p256dh: string;
+      auth: string;
+    };
+  };
+
 export async function POST(req: NextRequest) {
 
   try {
@@ -26,11 +34,20 @@ export async function POST(req: NextRequest) {
         icon: '/assets/icons/icon-192x192.png',
         badge: '/assets/icons/icon-192x192.png',
       };
-      if(subscription.length > 0) {
-          console.log('subscription', subscription);
-          await webPush.sendNotification(subscription[0], JSON.stringify(notificationPayload));
-          return NextResponse.json({ message: 'success' }, { status: 200 });
-      }
+      const rawSubscription = subscription[0].subscription;
+
+      // 데이터가 PushSubscription 타입에 부합하는지 확인합니다.
+      const pushSubscription: SubscriptionType = {
+        endpoint: rawSubscription.endpoint,
+        keys: {
+          p256dh: rawSubscription.keys.p256dh,
+          auth: rawSubscription.keys.auth,
+        },
+      };
+        
+        console.log('subscription', subscription);
+        await webPush.sendNotification(pushSubscription, JSON.stringify(notificationPayload));
+        return NextResponse.json({ message: 'success' }, { status: 200 });
     }
   } catch (error) {
     console.error('Unexpected error:', error);
