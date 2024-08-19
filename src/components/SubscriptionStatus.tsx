f5285ade2b9994b563f0f560a0c07f6753ef34be
mnmhbbb <66292371+mnmhbbb@users.noreply.github.com>
2024-08-19 07:51:29 +0000

    fix: env 수정

--- src/components/SubscriptionStatus.tsx
+++ src/components/SubscriptionStatus.tsx
@@ -36,7 +36,7 @@ const SubscriptionStatus = () => {
   }, []);
 
   const generateSubscribeEndPoint = async (newRegistration: ServiceWorkerRegistration) => {
-    const applicationServerKey = urlB64ToUint8Array(process.env.NEXT_PUBLIC_VAPID_KEY!);
+    const applicationServerKey = urlB64ToUint8Array(process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!);
     const options = {
       applicationServerKey,
       userVisibleOnly: true,
