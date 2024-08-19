c579ad3f7a6a37b2cb5fd277108d33d805fe466b
mnmhbbb <66292371+mnmhbbb@users.noreply.github.com>
2024-08-19 04:31:37 +0000

    next-pwa 제거

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
-import nextPwa from "next-pwa";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
-const withPWA = nextPwa({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  customWorkerDir: "worker",
-  // disable: process.env.NODE_ENV === 'development'
-  // runtimeCaching,
-});
-
-const config = withPWA({
-  ...nextConfig,
-});
 
 export default nextConfig;
