e8d9ea9b4b71e21db19fcf203344b0956c4956e8
mnmhbbb <mnmhb64@gmail.com>
2024-09-03 01:01:44 +0900

    feat: supabase-js localStorage 저장으로 변경

--- src/utils/supabase/supabase-js.ts
+++ src/utils/supabase/supabase-js.ts
@@ -6,7 +6,7 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 const createSupabaseJs = () =>
   createClient(supabaseUrl, supabaseAnonKey, {
     auth: {
-      persistSession: false,
+      // persistSession: false,
     },
   });
 
