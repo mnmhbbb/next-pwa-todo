d820ca4a20639b9cba2be91b28339e9d306e554e
mnmhbbb <mnmhb64@gmail.com>
2024-09-05 01:33:06 +0900

    feat: 로딩 스피너 스타일 수정

--- src/components/GlobalLoadingSpinner.tsx
+++ src/components/GlobalLoadingSpinner.tsx
@@ -8,7 +8,7 @@ const GlobalLoadingSpinner = () => {
 
   return isLoading ? (
     <div className="fixed inset-0 flex justify-center items-center bg-gray-800 bg-opacity-50 z-50">
-      <div className="w-12 h-12 border-4 border-dashed rounded-full animate-spin border-blue-500"></div>
+      <div className="w-12 h-12 border-4 border-solid rounded-full animate-spin border-blue-500"></div>
     </div>
   ) : null;
 };
