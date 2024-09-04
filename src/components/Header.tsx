d9dc216f9c87b5e4a93b2539e47970eb284f4ee4
mnmhbbb <mnmhb64@gmail.com>
2024-09-05 00:34:30 +0900

    feat: Header 컴포넌트 분리

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <Link className="flex justify-center py-5" href="/">
+      Home
+    </Link>
+  );
+};
+
+export default Header;
