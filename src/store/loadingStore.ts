f85f4f67714119d37bb8835b996456a52a6f1abe
mnmhbbb <mnmhb64@gmail.com>
2024-09-05 00:04:40 +0900

    feat: 로딩 스토어 간단하게 변경
    - 로딩 키로 분리하는 로직은 이후에 더 발전시키기
    - 우선은 이렇게 간단하게 하고 넘어가기

--- src/store/loadingStore.ts
+++ src/store/loadingStore.ts
@@ -1,33 +1,11 @@
-import { create, StateCreator } from "zustand";
-import { devtools } from "zustand/middleware";
-
-type LoadingKey = "AUTH"; // enum처럼 사용
+import { create } from "zustand";
 
 interface LoadingState {
-  loadingStates: Record<LoadingKey, boolean>;
-  setLoading: (key: LoadingKey, isLoading: boolean) => void;
-  isLoading: (key: LoadingKey) => boolean;
-  isAnyLoading: () => boolean;
+  isLoading: boolean;
+  setLoading: (isLoading: boolean) => void;
 }
 
-const storeCreator: StateCreator<LoadingState> = (set, get) => ({
-  loadingStates: {
-    AUTH: false,
-  },
-  setLoading: (key, isLoading) =>
-    set((state) => {
-      if (state.loadingStates[key] === isLoading) return state; // 상태가 변경되지 않은 경우 set 호출 방지
-      return {
-        loadingStates: {
-          ...state.loadingStates,
-          [key]: isLoading,
-        },
-      };
-    }),
-  isLoading: (key) => get().loadingStates[key] || false,
-  isAnyLoading: () => Object.values(get().loadingStates).some(Boolean),
-});
-
-export const useLoadingStore = create<LoadingState>()(
-  devtools(storeCreator, { name: "loadingStore" }),
-);
+export const useLoadingStore = create<LoadingState>((set) => ({
+  isLoading: false,
+  setLoading: (isLoading) => set({ isLoading }),
+}));
