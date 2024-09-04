f21ccb9d19928ffd3cb314c3eeea56111259972b
mnmhbbb <mnmhb64@gmail.com>
2024-09-05 01:33:47 +0900

    feat: 메인 페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,30 +3,20 @@
 import SubscriptionStatus from "@/components/SubscriptionStatus";
 import { useAuth } from "@/hooks/useAuth";
 import Link from "next/link";
-import { useEffect } from "react";
 
 export default function Home() {
-  const { user, isLoggedIn, isLoading, isInitialized } = useAuth();
-
-  useEffect(() => {
-    if (isInitialized) {
-      console.log("Auth initialized - User:", user, "IsLoggedIn:", isLoggedIn);
-    }
-  }, [isInitialized, user, isLoggedIn]);
-
-  if (!isInitialized || isLoading()) {
-    return (
-      <main className="flex justify-center items-center h-screen">
-        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
-      </main>
-    );
-  }
+  const { user, isLoggedIn, logout } = useAuth();
 
   return (
     <main className="flex flex-col items-center justify-between p-12">
       <div className="flex gap-5 mb-10">
         {!isLoggedIn && <Link href="/login">로그인</Link>}
-        {isLoggedIn && <p>환영합니다, {user?.email}!</p>}
+        {isLoggedIn && (
+          <>
+            <p>환영합니다, {user?.email}!</p>
+            <button onClick={logout}>로그아웃</button>
+          </>
+        )}
       </div>
       <SubscriptionStatus />
     </main>
