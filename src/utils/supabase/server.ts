54a1a4925e8cf53228faa7fd184b3b3f6700e45c
mnmhbbb <mnmhb64@gmail.com>
2024-09-09 21:13:01 +0900

    feat: supabase server 설정에 role key 삭제

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,13 +1,11 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export default function createClient(useServiceRole = false) {
+export default function createClient() {
   const cookieStore = cookies();
 
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-  const supabaseKey = useServiceRole
-    ? process.env.SUPABASE_SERVICE_ROLE_KEY!
-    : process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
   // Create a server's supabase client with newly configured cookie,
   // which could be used to maintain user's session
